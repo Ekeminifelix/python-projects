f2f351f88596dc3df16344e7d675c3eaafd7e612
Ekemini Felix <felixekemini9@gmail.com>
2025-04-03 16:35:40 +0100

    first file commit

---
+++ Python Project1.py
@@ -0,0 +1,31 @@
+name = input ("Please enter your name: ")
+print ("Haloo", name, "welcome to my game!")
+
+should_we_play = input("Do you want to play? ").lower()
+if should_we_play == "yes" or should_we_play == 'y':
+    print("We are going to play!")
+
+    direction = input("Do you want to go left or right? (left/right) ").lower()
+    if direction =="left":
+        print("You went to the wrong direction and you fall off the cliff. GAME OVER!! ")
+    elif direction == "right":
+        choice = input ("Okay, you now on the bridge, do you want to swim or cross the bridge? (swim/cross) ").lower()
+        if choice == swim:
+            print("you've just been eaten by an alligator. GAME OVER!!! ")
+        elif choice == cross:
+            pick = input ("Okay, you are close uncovering the treasure. Do you want to run or drive? (run/drive) ").lower()
+            if pick == run:
+                print("sorry, your energy is low and you have been eliminated. Better luck next time ")
+            elif pick == drive:
+                print("You have found the hidden treasure. CONGRATULATIONS")
+                
+    
+    else:
+        print("sorry!! this is an invalid reply")
+
+else:
+    print("So sad we will not be playing")
+
+
+
+
